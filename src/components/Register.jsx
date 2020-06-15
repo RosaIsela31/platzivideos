35f082a76701f2c4d8f731b396355c4d0a7bfe59
Rosa <rosaislp@gmail.com>
2020-06-15 11:46:15 -0500

    working with login and gravatar

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,21 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import '../styles/Register.css'
 
-const Register = () => (
-  <section className="register">
-    <section className="register__container">
-      <h2>Regístrate</h2>
-      <form className="register__container--form">
-        <input className="input" type="text" placeholder="Nombre" />
-        <input className="input" type="text" placeholder="Correo" />
-        <input className="input" type="password" placeholder="Contraseña" />
-        <button className="button">Registrarme</button>
-      </form>
-      <Link to="/login">Iniciar sesión</Link>
-    </section>
-  </section>
-);
+const Register = () => {
 
-export default Register;
+  const [form, setValues] = useState({
+    email: '',
+    name: '',
+    password: '',
+  });
+
+  const handleInput = event => {
+    setValues({
+      ...form,
+      [event.target.name]: event.target.value,
+    })
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault(); 
+    console.log('form', form);
+    
+  }
+
+  return (
+    <section className="register">
+      <section className="register__container">
+        <h2>Regístrate</h2>
+        <form className="register__container--form" onSubmit={handleSubmit}>
+          <input 
+            name="name"
+            className="input" 
+            type="text" 
+            placeholder="Nombre" 
+            onChange={handleInput}
+          />
+          <input 
+            name="email"
+            className="input" 
+            type="text" 
+            placeholder="Correo" 
+            onChange={handleInput}
+          />
+          <input
+            name="password" 
+            className="input" 
+            type="password" 
+            placeholder="Contraseña" 
+            onChange={handleInput}
+          />
+          <button 
+            className="button">Registrarme
+          </button>
+        </form>
+        <Link to="/login">Iniciar sesión</Link>
+      </section>
+    </section>
+)};
+
+export default connect(null, null )(Register);
